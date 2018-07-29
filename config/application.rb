522095eb9d908ea1cd0c414b9222a0b5e200734b
Sunny <sjbeatteay@gmail.com>
2018-07-29 14:30:11 -0400

    remove cookies and sessions

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module IrisPortfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    config.middleware.delete ActionDispatch::Cookies
+    config.middleware.delete ActionDispatch::Session::CookieStore
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
